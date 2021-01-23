06c60f8d5ed5a8868099e90a56e0f8102b345676
Yurii Bezruk <yurii.bezruk.it@gmail.com>
2021-01-23 12:40:07 +0200

    Resolving commit problem with deleted classes...

---
+++ src/front/MainFrame.java
@@ -0,0 +1,12 @@
+package front;
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 2269971701250845501L;
+	private JPanel mainPanel;
+	
+	public MainFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+}
