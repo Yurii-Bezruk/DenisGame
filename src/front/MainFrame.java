6f28a23b107f7823eefe197a04bee59a80d0025e
Dibtseva-Anna <61124401+Dibtseva-Anna@users.noreply.github.com>
2021-01-31 13:07:47 +0200

    Add files via upload

--- src/front/MainFrame.java
+++ src/front/MainFrame.java
@@ -1,4 +1,7 @@
 package front;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
@@ -6,7 +9,20 @@ public class MainFrame extends JFrame {
 	private JPanel mainPanel;
 	
 	public MainFrame() {
+		//creating a window with name
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		//adjusting window size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize((int)(screenSize.width/1.5), (int)(screenSize.height/1.5));
+		setLocationRelativeTo(null);
+		//setBounds(screenSize.width/6, screenSize.height/6, (int)(screenSize.width/1.5), (int)(screenSize.height/1.5));
+		
+		setVisible(true);
 	}
+	
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+	}
+	
 }
